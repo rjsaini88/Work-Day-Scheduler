3ed97d33941e0f8457f00b8a4d20e2b8bf242dea
Rj Saini <rsaini88@gmail.com>
2022-11-02 13:40:51 -0700

    Refactored work to use Jquery and bootstrap

--- Develop/script.js
+++ Develop/script.js
@@ -6,33 +6,33 @@ var container = document.querySelector(".container");
 var blocks = [9,10,11,12,13,14,15,16,17];
 var date = new Date();
 
-function currentHour(hour){
-    if (date.getHours() === hour) {
-        return 1}
-        else if (date.getHours() > hour){
-            return 2}
-else {return 3}
-        }
+// function currentHour(hour){
+//     if (date.getHours() === hour) {
+//         return 1}
+//         else if (date.getHours() > hour){
+//             return 2}
+// else {return 3}
+//         }
 
-for (var i = 0; i < blocks.length; i++){
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute ("class", "row timeblock");
-    if (currentHour(blocks[i]) ===1) {
-        wrapper.classList.add("present");
-    }else if (currentHour(blocks[i]) === 2){
-        wrapper.classList.add("past");}
-    else {
-        wrapper.classList.add("future");
-    }
+// for (var i = 0; i < blocks.length; i++){
+//     var wrapper = document.createElement("div");
+//     wrapper.setAttribute ("class", "row timeblock");
+//     if (currentHour(blocks[i]) ===1) {
+//         wrapper.classList.add("present");
+//     }else if (currentHour(blocks[i]) === 2){
+//         wrapper.classList.add("past");}
+//     else {
+//         wrapper.classList.add("future");
+//     }
     
-    var block = document.createElement("div");
-    var textbox = document.createElement("textarea");
-    textbox.setAttribute ("style", "flex-grow: 1; height:100px;");
-    block.textContent = moment(blocks[i], 'HH').format('h a');
-    wrapper.appendChild(block);
-    wrapper.appendChild(textbox);
-    container.appendChild(wrapper);
-}
+//     var block = document.createElement("div");
+//     var textbox = document.createElement("textarea");
+//     textbox.setAttribute ("style", "flex-grow: 1; height:100px;");
+//     block.textContent = moment(blocks[i], 'HH').format('h a');
+//     wrapper.appendChild(block);
+//     wrapper.appendChild(textbox);
+//     container.appendChild(wrapper);
+// }
 
 
 
