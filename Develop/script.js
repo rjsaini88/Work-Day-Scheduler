c45c47cdddd9f77e8b65be66b85623b950a51403
Rj Saini <rsaini88@gmail.com>
2022-11-06 23:34:14 -0800

    Added functioning JavaScript to the planner

--- Develop/script.js
+++ Develop/script.js
@@ -1,71 +1,78 @@
-console.log("Your script is loaded");
-var timeEl = $(".date");
-var timerInterval;
-// var containerEl = $(".container");
-var container = document.querySelector(".container");
-var blocks = [9,10,11,12,13,14,15,16,17];
-var date = new Date();
-var hours = date.getHours();
-console.log(date); //date, hours and minutes
-console.log(hours);//give me the hour 
-console.log (date + hours);
+$(document).ready(function () {
+  console.log("Your script is loaded");
+  var timeEl = $(".date");
+  var timerInterval;
+  // var containerEl = $(".container");
+  var container = document.querySelector(".container");
+  var blocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var date = new Date();
+  var currentHour = date.getHours() % 12 || 12;
+  console.log(date); //date, hours and minutes
+  console.log(currentHour); //give me the hour
+  console.log(date + currentHour);
+  $(".notification").hide();
 
-var startTimer = function() {
-    timerInterval = setInterval(function() {
-//this function gets called every second.
-//get the date time string every second and put on page. 
+  $("saveBtn").on("click", function (e) {
+    // question on save to localstorage and
+    // e.preventDefault();
+    var toDo = $(".description").value();
+    var time = $(".hour").text();
+    localStorage.setItem(time, toDo);
+    console.log(time);
+    setTimeout(function () {
+      $(".notification").show(); //question on how to make this function work on click. display for 5 seconds
+    }, 5000);
+  });
 
-var currentDate = moment().format('dddd MMMM Do [at] LT'); //hh:mm:ss a
-timeEl.text(currentDate);
-        },1000);
-    };
-    startTimer();
+  var startTimer = function () {
+    timerInterval = setInterval(function () {
+      //this function gets called every second.
+      //get the date time string every second and put on page.
 
+      var currentDate = moment().format("dddd MMMM Do [at] LT"); //hh:mm:ss a
+      timeEl.text(currentDate);
+    }, 1000);
+  };
+  startTimer();
 
+  var appHour = parseInt($(".hour").text());
+  console.log(appHour);
 
+  console.log(parseInt($(".hour").text()));
 
-  var htmlVar = document.getElementsByClassName (".hour");
+  $(".hour").each(function () {
+    var appHour = parseInt($(this).text());
+    console.log(appHour);
+
+    if (appHour < currentHour) {
+      $(".time-block").addClass("past");
+    } else if (appHour === currentHour) {
+      $(".time-block").addClass("present");
+      $(".time-block").removeClass("past");
+    } else {
+      $(".time-block").addClass("future");
+      $(".time-block").removeClass("present");
+      $(".time-block").removeClass("past");
+    }
+  });
+  var htmlVar = document.getElementsByClassName(".hour");
   var text = htmlVar.textContent;
 
-  for (var i = 0; i < text.length; i++) {
-    console.log(text[i])
-  }
+  //   for (var i = 0; i < text.length; i++) {
+  //     console.log(text[i])
 
-
-
-  $(".hour").text()
-
-// function currentHour(hour){
-//     if (date.getHours() === hour) {
-//         return 1}
-//         else if (date.getHours() > hour){
-//             return 2}
-// else {return 3}
-//         }
-
-// for (var i = 0; i < blocks.length; i++){
-//     var wrapper = document.createElement("div");
-//     wrapper.setAttribute ("class", "row timeblock");
-//     if (currentHour(blocks[i]) ===1) {
-//         wrapper.classList.add("present");
-//     }else if (currentHour(blocks[i]) === 2){
-//         wrapper.classList.add("past");}
-//     else {
-//         wrapper.classList.add("future");
-//     }
-    
-//     var block = document.createElement("div");
-//     var textbox = document.createElement("textarea");
-//     textbox.setAttribute ("style", "flex-grow: 1; height:100px;");
-//     block.textContent = moment(blocks[i], 'HH').format('h a');
-//     wrapper.appendChild(block);
-//     wrapper.appendChild(textbox);
-//     container.appendChild(wrapper);
-// }
-
-
-
-
-
-
-  
+  /*GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar - done
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours - done
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+*/
+});
